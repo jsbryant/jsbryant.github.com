cb3e885a2bbab9f06a15d65a529e059572967ada
Jordan Bryant <jsbrymail@gmail.com>
2017-07-02 11:14:16 -0700

    Delete <script> tags

--- js/main.js
+++ js/main.js
@@ -1,43 +1,37 @@
-<!-- Reload to top of page -->
-	<script type="text/javascript">
-		window.onbeforeunload = function () {
-		  window.scrollTo(0, 0);
-		}
-	</script>
+// Reload to top of page 
+	window.onbeforeunload = function () {
+		window.scrollTo(0, 0);
+	}
 
-	<!-- Sticky Header -->
-	<script type="text/javascript">
-		$('#header').stickThis();
-	</script>
+// Sticky Header
+	$('#header').stickThis();
 
-	<!-- Big ups Chris Coyier https://css-tricks.com/snippets/jquery/smooth-scrolling/ -->
-	<script type="text/javascript">
-		$('a[href*="#"]')
-		  .not('[href="#"]')
-		  .not('[href="#0"]')
-		  .click(function(event) {
-		    if (
-		      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-		      && 
-		      location.hostname == this.hostname
-		    ) {
-		      var target = $(this.hash);
-		      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		      if (target.length) {
-		        event.preventDefault();
-		        $('html, body').animate({
-		          scrollTop: target.offset().top
-		        }, 1000, function() {
-		          var $target = $(target);
-		          $target.focus();
-		          if ($target.is(":focus")) {
-		            return false;
-		          } else {
-		            $target.attr('tabindex','-1');
-		            $target.focus();
-		          };
-		        });
-		      }
-		    }
-		  });
-	</script>
+// Big ups Chris Coyier https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	$('a[href*="#"]')
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      if (target.length) {
+		event.preventDefault();
+		$('html, body').animate({
+		  scrollTop: target.offset().top
+		}, 1000, function() {
+		  var $target = $(target);
+		  $target.focus();
+		  if ($target.is(":focus")) {
+		    return false;
+		  } else {
+		    $target.attr('tabindex','-1');
+		    $target.focus();
+		  };
+		});
+	      }
+	    }
+	  });
